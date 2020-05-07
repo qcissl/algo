1ac6941fc7bfd4fcc67ff0f8646735cac66302d0
qincheng <qincheng@weijie.com.cn>
2020-05-07 13:55:11 +0800

    优化LinkedList

---
+++ src/main/test/com/qc/algo/linkedlist/MyLinkedListTest.java
@@ -0,0 +1,13 @@
+package com.qc.algo.linkedlist;
+
+/**
+ * description
+ *
+ * @author qinc 2020/05/07 13:54
+ */
+public class MyLinkedListTest {
+
+    public static void main(String[] args) {
+
+    }
+}
